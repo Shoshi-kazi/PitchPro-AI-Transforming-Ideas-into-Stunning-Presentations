8318aad9b91d0c1a46f5e28c9b84734ff95f0f5e
user <nusaibajannatshoshi@gmail.com>
2024-11-21 19:37:52 +0600

    Added Presentation

--- Presentation.java
+++ Presentation.java
@@ -4,32 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Presentation {
-private String title;
-private Template template;
-private List<String> slides;
+    private String title;
+    private Template template;
+    private List<String> slides;
 
-public Presentation(String title, Template template) {
-this.title = title;
-this.template = template;
-this.slides = new ArrayList<>();
+    public Presentation(String title, Template template) {
+        this.title = title;
+        this.template = template;
+        this.slides = new ArrayList<>();
+    }
+
+    public void addSlide(String slide) {
+        slides.add(slide);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Template getTemplate() {
+        return template;
+    }
+
+    public List<String> getSlides() {
+        return new ArrayList<>(slides); // Return a copy to prevent external modification
+    }
+
+    public void displaySlides() {
+        System.out.println("Slides for: " + title);
+        for (String slide : slides) {
+            System.out.println("- " + slide);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Presentation{" +
+                "title='" + title + '\'' +
+                ", template=" + template +
+                ", slides=" + slides +
+                '}';
+    }
 }
 
-public void addSlide(String slide) {
-slides.add(slide);
-}
-
-public String getTitle() {
-return title;
-}
-
-public List<String> getSlides() {
-return slides;
-}
-
-public void displaySlides() {
-System.out.println("Slides for: " + title);
-for (String slide : slides) {
-System.out.println("- " + slide);
-}
-}
-}
