47658c1b76ebb075348888fca4d5ccb35c3ec34e
user <nusaibajannatshoshi@gmail.com>
2024-11-21 19:27:16 +0600

    Added User

--- User.java
+++ User.java
@@ -1,12 +1,16 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class User {
     private String username;
     private String email;
+    private LocalDateTime createdAt;
 
     public User(String username, String email) {
         this.username = username;
         this.email = email;
+        this.createdAt = LocalDateTime.now(); 
     }
 
     public String getUsername() {
@@ -16,5 +20,19 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
+
 
