853b81304be85d787d266d6aad96466547e69c61
user <nusaibajannatshoshi@gmail.com>
2024-11-27 09:37:20 +0600

    Update Slide Generation.java

--- Slide Generation.java
+++ Slide Generation.java
@@ -1,8 +1,16 @@
-package services;
+        // Ask for Topic
+        System.out.println("\n\u001B[36mEnter the topic for the presentation: \u001B[0m");
+        String topic = scanner.nextLine();
 
-public class DetailedContentGenerator implements ContentGenerator {
-    @Override
-    public void generateContent(String topic) {
-        System.out.println("Generating detailed content for: " + topic);
+        // Create Content Generator
+        ContentGenerator contentGenerator = new AIContentGenerator();
+        List<Slide> slides = contentGenerator.generateSlides(topic);
+
+        // Display Slide Content
+        System.out.println("\n\u001B[1m-------------------------------------\u001B[0m");
+        System.out.println("\u001B[1mGenerated Slides for \u001B[32m" + topic + ":\u001B[0m");
+        System.out.println("\u001B[1m-------------------------------------\u001B[0m");
+        for (Slide slide : slides) {
+            System.out.println(slide);
+        }
     }
-}
