d0ed842c6373bf8c6eef6e95858a3155b2dd1965
user <nusaibajannatshoshi@gmail.com>
2024-11-27 09:38:45 +0600

    Update ContentGenerator Class.java

--- ContentGenerator Class.java
+++ ContentGenerator Class.java
@@ -1,5 +1,20 @@
-package services;
+abstract class ContentGenerator {
+    public abstract List<Slide> generateSlides(String topic);
+}
 
-public interface ContentGenerator {
-    void generateContent(String topic);
+class AIContentGenerator extends ContentGenerator {
+    @Override
+    public List<Slide> generateSlides(String topic) {
+        List<Slide> slides = new ArrayList<>();
+        slides.add(new Slide(1, "Introduction to " + topic));
+        slides.add(new Slide(2, "Applications of " + topic));
+        slides.add(new Slide(3, "Key Features of " + topic));
+        slides.add(new Slide(4, "Challenges and Future Directions in " + topic));
+        slides.add(new Slide(5, "Historical Context of " + topic));
+        slides.add(new Slide(6, "Case Studies Related to " + topic));
+        slides.add(new Slide(7, "Comparisons with Similar Topics"));
+        slides.add(new Slide(8, "Implementation Strategies for " + topic));
+        slides.add(new Slide(9, "Summary and Key Takeaways"));
+        return slides;
+    }
 }
